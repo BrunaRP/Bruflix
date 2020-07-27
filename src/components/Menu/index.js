0f4b5e3dcd33e4501e59244e3363dc0354bfdd9e
Bruna Polonio <polonio.bruna@gmail.com>
2020-07-27 16:54:15 -0300

    componente_menu_e_css

---
+++ src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+
+function Menu(){
+    return(
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Bruflix logo"/> 
+            </a>
+        </nav>
+    );
+     
+}
+
+export default Menu; //pra poder usar em outro lugares
