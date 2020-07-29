229cf6030b5e02a2feee6baa6e9e2380a02c05c3
Bruna Polonio <polonio.bruna@gmail.com>
2020-07-29 16:54:14 -0300

    app vira Home, criação de um Page Default e componente de cadastro de video

---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+
+    function CadastroVideo(){
+        return(
+          <PageDefault>
+            Página de cadastro de vídeo
+          </PageDefault>
+        )
+      }
+
+export default CadastroVideo;
