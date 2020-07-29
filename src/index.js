7d4353c48b6cd71f77aee90d0d052ce5aab32717
Bruna Polonio <polonio.bruna@gmail.com>
2020-07-29 17:33:32 -0300

    criação de componente categoria do cadastro, ajuste no css geral e do PageDefault

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import './index.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Home from './pages/Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 
 ReactDOM.render(
   <BrowserRouter>
   <Switch>
+    <Route path="/" component={Home}/>
     <Route path= "/cadastro/video" component={CadastroVideo}/>
-    <Route path="/" component={Home} />
-
+    <Route path= "/cadastro/categoria" component={CadastroCategoria}/>
   </Switch>
   </BrowserRouter>,
   document.getElementById('root')
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroVideo(){
+function CadastroCategoria(){
   return(
     <PageDefault>
-      <h1>Cadastro de vídeo</h1> 
+      <h1>Cadastro de categoria</h1> 
          
-      <Link to="/cadastro/categoria">
-        Cadastrar Categoria
+      <Link to="/">
+        ir para home
       </Link>
    </PageDefault>
   );
 }
 
-export default CadastroVideo;
+export default CadastroCategoria;
