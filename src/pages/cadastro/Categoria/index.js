71229b40d9120de16c249bb9da96bd32f357dc01
Bruna Polonio <polonio.bruna@gmail.com>
2020-07-30 16:48:11 -0300

    criação do slider e form basico em cadastro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,15 +2,30 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroCategoria(){
-  return(
+function CadastroCategoria() {
+  return (
     <PageDefault>
-      <h1>Cadastro de categoria</h1> 
-         
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
       <Link to="/">
-        ir para home
+        Ir para home
       </Link>
-   </PageDefault>
+    </PageDefault>
   )
 }
 
